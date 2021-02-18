7a1543dd38469430721508f42c5e5c3b45ebd7d2
munchkinhalfling <munchkinhalfling@pm.me>
2021-02-18 16:08:58 -0500

    The rover has landed!

--- perseverance/__init__.py
+++ perseverance/__init__.py
@@ -1,6 +1,3 @@
-# Taken from https://codegolf.stackexchange.com/questions/114960/when-will-brexit-happen
-from datetime import*
-# This is the main file.
+
 def main():
-    x = datetime
-    while 1:d=x(2021,2,18,15,55)-x.now();s=d.seconds;a=s%3600;print(end=f"\r{d.days:03}:{s//3600:02}:{a//60:02}:{s%60:02}")
+    print("\33[1;32mPerseverance Has Landed!\33[0m")
