35872488964e597ef6bbd896249acf61b70ee63b
munchkinhalfling <munchkinhalfling@pm.me>
2021-02-18 15:29:29 -0500

    Countdown to Mars!

---
+++ perseverance/__init__.py
@@ -0,0 +1,6 @@
+from rich.progress import track
+from datetime import*
+# This is the main file.
+def main():
+    x = datetime
+    while 1:d=x(2021,2,18,15,55)-x.now();s=d.seconds;a=s%3600;print(end=f"\r{d.days:03}:{s//3600:02}:{a//60:02}:{s%60:02}")
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name="perseverance",
+    version="master",
+    author="munchkinhalfling",
+    # author_email="",
+    # url="",
+    description="Countdown to Mars",
+    packages=["perseverance"],
+    scripts=['perseverance-ctm']
+)
